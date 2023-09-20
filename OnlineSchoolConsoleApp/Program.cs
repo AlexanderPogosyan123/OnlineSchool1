e46bcd5b973219b80d8c62e4c4d4792753e13bac
Alexander Pogosyan <aleksandrPogosyan76@gmail.com>
2023-09-20 21:36:27 +0400

    Add project files.

---
+++ OnlineSchoolConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineSchoolConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
